1d2397290b3941c74f6cec475392c5116025766c
Akintola Ramota <akintolarahmah@gmail.com>
2020-06-15 23:12:04 +0000

    user validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@
 class User < ApplicationRecord
   has_many :comments
   validates :user_name, presence: true,
-                        length: { minimum: 3, maximum: 10 }
+                        length: { minimum: 3, maximum: 15 }
 end
