bff50aceae38064f3c29f891ac0dd2ac56d30213
Akintola Ramota <akintolarahmah@gmail.com>
2020-06-15 23:46:18 +0000

    validation added for post models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200615225624) do
+ActiveRecord::Schema.define(version: 20200615233728) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20200615225624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
